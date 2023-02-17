2b6f7d636a008d9298db60a83dae743986e1af74
rekhan <rekhan@expediagroup.com>
2023-02-17 11:58:12 +0000

    all of MusciansAndBandPart1 completed, written test for updating and deleting instance

--- index.test.js
+++ index.test.js
@@ -1,6 +1,7 @@
 const sequelize = require("./src/connection");
 const { Bands } = require("./models/Bands");
 const { Musicians } = require("./models/Musicians");
+const { where } = require("sequelize");
 
 beforeAll(async () => {
   await sequelize.sync({ force: true });
@@ -31,5 +32,51 @@ describe("testing bands/Musicians functionality and relationships", () => {
     });
 
     expect(musicians.name).toBe("Rejwan");
+  });
+
+  it("test whether you can delete an Instance", async () => {
+    await Bands.bulkCreate([
+      { name: "BTS", genre: "KPOP", showCount: 8 },
+      { name: "ImagineDragons", genre: "POP", showCount: 4 },
+      { name: "Linkin Park", genre: "Rock", showCount: 6 },
+    ]);
+
+    const allBands = await Bands.findAll();
+    console.log(allBands);
+
+    // Destroying BTS instance
+    await Bands.destroy({ where: { name: "BTS" } });
+    const Bts = await Bands.findOne({ where: { name: "BTS" } });
+    expect(Bts).toBeFalsy();
+
+    //ImagineDragons should still remain
+    const ImagineDragons = await Bands.findOne({
+      where: { name: "ImagineDragons" },
+    });
+    expect(ImagineDragons.name).toEqual("ImagineDragons");
+  });
+
+  //testing whether you can update an Instance
+
+  it.only("testing whether you can update an instance", async () => {
+    await Musicians.bulkCreate([
+      { name: "Rejwan", instrument: "Piano" },
+      { name: "Diogo", instrument: "Violin" },
+      { name: "John", instrument: "Trumpet" },
+    ]);
+
+    await Musicians.findOne({ where: { name: "Diogo" } });
+    const Diogo = await Musicians.findOne({ where: { name: "Diogo" } });
+    expect(Diogo.name).toEqual("Diogo");
+
+    await Musicians.update({ name: "Fatima" }, { where: { name: "Diogo" } });
+    const Fatima = await Musicians.findOne({ where: { name: "Fatima" } });
+
+    const seeIfDiogoUpdated = await Musicians.findOne({
+      where: { name: "Diogo" },
+    });
+    expect(seeIfDiogoUpdated).toBeFalsy();
+
+    expect(Fatima.name).toEqual("Fatima");
   });
 });
